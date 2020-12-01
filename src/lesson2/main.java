ed7c2cd5d0cc879c39e880c2f717444c2a32dd5a
Hekit47 <hekit@mail.ru>
2020-12-01 15:52:25 +0300

    Исправлено решение задачи 7**. Условие на сайте отличалось от условия в методичке (на сайте отсутствовал пример сдвига)

--- src/lesson2/main.java
+++ src/lesson2/main.java
@@ -2,196 +2,160 @@ package lesson2;
 
 public class main {
 
-            public static void main (String[] args)
-        {
-            // Задачи закрыты двойным слешем. Чтобы активировать задачу, в методе main уберите двойной слеш в напротив названия
-            // задача №1
-            //massivZamenaZnacheniy ();
-            // задача №2
-            //fillEmptyArray ();
-            // задача №3
-            //numberUnderSix ();
-            // задача №4
-            //fillArrayDiagonal ();
-            // задача №5
-           // minMax ();
-            // задача №6
-            //checkBalance ();
-            //moveArray ();
+    public static void main(String[] args) {
+        // Задачи закрыты двойным слешем. Чтобы активировать задачу, в методе main уберите двойной слеш в напротив названия
+        // задача №1
+        //massivZamenaZnacheniy ();
+        // задача №2
+        //fillEmptyArray ();
+        // задача №3
+        //numberUnderSix ();
+        // задача №4
+        //fillArrayDiagonal ();
+        // задача №5
+        // minMax ();
+        // задача №6
+        //checkBalance ();
+        moveArray();
 
-        }
-
-        public static void massivZamenaZnacheniy()
-        {
-            System.out.println("Задача 1");
-            int[] arr = {1,1,0,1,0,0,1,1,1,1};
-            for (int k = 0; k < arr.length; k ++)   // запускаем перебор в рамках массива
-            {
-                if (arr[k] == 1)                    // если условие выполняется, то присваиваем переменной другое значение 0
-                {
-                    arr[k] = 0;
-                }
-                else arr[k] = 1;                    //если условие не выполняется, то присваиваем переменной другое значение 1
-                System.out.println(arr[k]);
-            }
-
-        }
-
-        public static void fillEmptyArray()
-        {
-            System.out.println("Задача 2");
-            int [] arr = new int[8];
-            int i = 3;                             // задаем шаг увеличения
-            for (int k = 0; k < 8; k ++ )
-            {
-                arr[k] = i*k;
-                System.out.println(arr[k]);
-            }
     }
 
-        public static void numberUnderSix()
+    public static void massivZamenaZnacheniy() {
+        System.out.println("Задача 1");
+        int[] arr = {1, 1, 0, 1, 0, 0, 1, 1, 1, 1};
+        for (int k = 0; k < arr.length; k++)   // запускаем перебор в рамках массива
         {
-            System.out.println("Задача 3");
-            int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-            for (int k = 0; k < arr.length; k ++)
+            if (arr[k] == 1)                    // если условие выполняется, то присваиваем переменной другое значение 0
             {
-                if (arr[k] < 6)                     //  если число в текущей ячейке удовлетворяет условиям, то значение увеличиваем в 2 раза
-                {
-                    arr[k] = arr[k] * 2;
-                    System.out.println(arr[k]);
-                }
-                else System.out.println(arr[k]);
-            }
+                arr[k] = 0;
+            } else
+                arr[k] = 1;                    //если условие не выполняется, то присваиваем переменной другое значение 1
+            System.out.println(arr[k]);
         }
 
-        public static void fillArrayDiagonal()
-        {
-            System.out.println("Задача 4");
-            int [][] arr = new int[5][5];
-            for (int i = 0; i < arr.length; i ++)               // подбор переключается по столбцам массива
+    }
+
+    public static void fillEmptyArray() {
+        System.out.println("Задача 2");
+        int[] arr = new int[8];
+        int i = 3;                             // задаем шаг увеличения
+        for (int k = 0; k < 8; k++) {
+            arr[k] = i * k;
+            System.out.println(arr[k]);
+        }
+    }
+
+    public static void numberUnderSix() {
+        System.out.println("Задача 3");
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int k = 0; k < arr.length; k++) {
+            if (arr[k] < 6)                     //  если число в текущей ячейке удовлетворяет условиям, то значение увеличиваем в 2 раза
             {
-                for (int j = 0; j < arr.length; j ++)           // подбор перебирает значение внутри выбранного столбца
+                arr[k] = arr[k] * 2;
+                System.out.println(arr[k]);
+            } else System.out.println(arr[k]);
+        }
+    }
+
+    public static void fillArrayDiagonal() {
+        System.out.println("Задача 4");
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < arr.length; i++)               // подбор переключается по столбцам массива
+        {
+            for (int j = 0; j < arr.length; j++)           // подбор перебирает значение внутри выбранного столбца
+            {
+                if (i == j)                                 // в диагонали ячейки имеют зеркальный номер i и j
                 {
-                    if (i == j)                                 // в диагонали ячейки имеют зеркальный номер i и j
-                    {
-                        arr[i][j] = 1;
-                        System.out.print(arr[i][j] + " ");
+                    arr[i][j] = 1;
+                    System.out.print(arr[i][j] + " ");
+                } else if (j == arr.length - 1 - i)       // или номер одной ячейки должен быть ниже на размер второй от максимального
+                {
+                    arr[i][j] = 1;
+                    System.out.print(arr[i][j] + " ");
+                } else System.out.print(arr[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void minMax() {
+        System.out.println("Задача 5*");
+        int[] arr = {2, 5, 12, 3, -2, 0, -1};
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i])                               //путем сравнения всех значений находим минимальное, если следующее меньше предыдущего, то оно занимает его место
+            {
+                min = arr[i];                                // аналогично с максимальным
+            }
+
+        }
+        System.out.println("Минимальное число массива " + min);
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Максимальное число массива " + max);
+
+    }
+
+    public static void checkBalance() {
+        System.out.println("Задача 6**");
+        int[] arr = {2, 2, 2, 1, 2, 2, 10, 1};
+        int summPt1 = 0;
+        int summPt2 = 0;
+        int arrLength = arr.length;
+        // включаем перебор по всему массиву
+        for (int j = 0; j < arrLength; j++) {
+            // ссумируем ячейки массива с начала
+            summPt1 = summPt1 + arr[j];
+            //System.out.println(summPt1);
+            // следующий перебор суммирует ячейки с конца, но кроме тех, которые уже проссумированы в первом переборе
+            for (int i = arrLength - 1; i > j; i--) {
+                summPt2 = summPt2 + arr[i];
+            }
+            //System.out.println(summPt2);
+            // сравниваем суммы, если они равны, то завершаем программу, если нет, то продолжаем перебор
+            // в процессе кол-во ячеек в summPt1 увеличивается, а в summPt2 уменьшается
+            if (summPt1 == summPt2) {
+                System.out.println(true);
+                break;
+            }
+            summPt2 = 0;
+        }
+
+    }
+
+    public static void moveArray() {
+        System.out.println("Задача 7***");
+        int[] arr = {1, 2, 3, 4, 5};
+        int move = -7;
+        // определяем, по какому маршруту пойдёт число
+        if (move >= 0 ) {
+            for (int i = 0; i < move; i++) {
+                // смещаем значения на размер сдвига
+                int x = arr[arr.length - 1];
+                for (int j = arr.length - 1; j >= 1; j--) {
+                    arr[j] = arr[j - 1];
+                }
+                arr[0] = x;
+            }
+
+        } else
+            move = move * -1;
+                for (int i = 0; i < move; i++) {
+                    // смещаем значения на размер сдвига
+                    int x = arr[0];
+                    for (int j = 0; j <= arr.length - 2; j++) {
+                        arr[j] = arr[j + 1];
                     }
-                        else if (j == arr.length - 1 - i)       // или номер одной ячейки должен быть ниже на размер второй от максимального
-                    {
-                        arr[i][j] = 1;
-                        System.out.print(arr[i][j] + " ");
-                    }
-                        else System.out.print(arr[i][j] + " ");
-
-                }
-                System.out.println();
-            }
-        }
-
-        public static void minMax()
-        {
-            System.out.println("Задача 5*");
-            int [] arr = {2, 5, 12, 3, -2, 0, -1};
-            int min = arr[0];
-            int max = arr[0];
-            for (int i = 0; i < arr.length; i ++)
-            {
-                if (min > arr[i])                               //путем сравнения всех значений находим минимальное, если следующее меньше предыдущего, то оно занимает его место
-                {
-                   min = arr[i];                                // аналогично с максимальным
-                }
-
-            }
-            System.out.println("Минимальное число массива " + min);
-            for (int i = 0; i < arr.length; i ++)
-            {
-                if (max < arr[i])
-                {
-                    max = arr[i];
-                }
-            }
-            System.out.println("Максимальное число массива " + max);
-
-        }
-
-        public static void checkBalance ()
-        {
-            System.out.println("Задача 6**");
-            int [] arr = {2, 2, 2, 1, 2, 2, 10, 1};
-            int summPt1 = 0;
-            int summPt2 = 0;
-            int arrLength = arr.length;
-            // включаем перебор по всему массиву
-            for (int j = 0; j < arrLength; j ++)
-            {
-                // ссумируем ячейки массива с начала
-                summPt1 = summPt1 + arr[j];
-                //System.out.println(summPt1);
-                // следующий перебор суммирует ячейки с конца, но кроме тех, которые уже проссумированы в первом переборе
-                for (int i = arrLength - 1; i > j; i --)
-                {
-                    summPt2 = summPt2 + arr[i];
-                }
-                //System.out.println(summPt2);
-                // сравниваем суммы, если они равны, то завершаем программу, если нет, то продолжаем перебор
-                // в процессе кол-во ячеек в summPt1 увеличивается, а в summPt2 уменьшается
-                if (summPt1 == summPt2)
-                {
-                   System.out.println(true);
-                   break;
-                }
-                summPt2 = 0;
+                    arr[arr.length - 1] = x;
             }
 
-        }
 
-       public static void moveArray ()
-       {
-           System.out.println("Задача 7***");
-           int [] arr = {1, 2, 3, 4, 5};
-           int move = 2;
-           // определяем, по какому маршруту пойдёт число
-           if (move >= 0 && move <= arr.length)
-           {
-               // смещаем значения на размер сдвига
-               for (int j = arr.length - 1; j >= move; j--) {
-                   arr[j] = arr[j - move];
-               }
-               // обнуляем значение, которые в диапазоне сдвига
-               for (int i = 0; i < move; i++) {
-                   arr[i] = 0;
-               }
-               for (int a = 0; a < arr.length; a++) {
-                   System.out.print(arr[a]);
-               }
-           }
-           else if (move < 0 && move * -1 <= arr.length)
-           {
-               move = move * -1;
-               for (int i = 0; i < arr.length - move; i ++)
-               {
-                   arr[i] = arr[i + move];
-               }
-               for (int j = arr.length - 1; j > arr.length -1 - move; j --)
-               {
-                   arr[j] = 0;
-               }
-
-
-               for (int d = 0; d < arr.length; d++) {
-                   System.out.print(arr[d]);
-               }
-           }
-           else
-           {
-               // если сдвиг больше размера массива, то все значения массива равны 0, т.к. сдвинулись за массив
-               for (int d = 0; d < arr.length; d++)
-               {
-                   arr[d] = 0;
-                   System.out.print(arr[d]);
-               }
-           }
-       }
-
+    }
 }
+
+
